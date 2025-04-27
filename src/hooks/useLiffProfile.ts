e8e408ea0d07198b4972c0686b7d77366e214a28
iNAS-lab <sinagaki612@gmail.com>
2025-04-27 09:19:58 +0000

    LINEログイン

--- src/hooks/useLiffProfile.ts
+++ src/hooks/useLiffProfile.ts
@@ -1,20 +1,22 @@
-// src/hooks/useLiffProfile.ts
 import { useEffect, useState } from 'react'
 import liff from '@line/liff'
 
-export function useLiffProfile() {
+export const useLiffProfile = () => {
   const [userId, setUserId] = useState<string | null>(null)
 
   useEffect(() => {
-    const fetchProfile = async () => {
+    const initLiff = async () => {
       await liff.init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID! })
-      if (!liff.isLoggedIn()) liff.login()
 
-      const profile = await liff.getProfile()
-      setUserId(profile.userId)
+      if (!liff.isLoggedIn()) {
+        liff.login({ redirectUri: window.location.href }) // ← ここで「今のページ」に戻す
+      } else {
+        const profile = await liff.getProfile()
+        setUserId(profile.userId)
+      }
     }
 
-    fetchProfile()
+    initLiff()
   }, [])
 
   return userId
